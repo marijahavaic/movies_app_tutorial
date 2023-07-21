3bf92cda880fd83116deec089d7ac8c48818c56c
Marija Havaic <marija.havaic@gmail.com>
2023-07-21 15:30:16 +0200

    Repository functions

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +11,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoviesController extends AbstractController
 {
     #[Route('/movies', name: 'app_movies')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-        $movies = ['Avengers: Endgame', "Inception", "Loki", "Black Widow"];
+        // findAll() - SELECT * FROM movies;
+        // find() - SELECT * FROM movies WHERE id = 5;
+        // findBy() - SELECT * FROM movies ORDER BY id DESC;
+        // findOneBy() - SELECT * FROM movies WHERE id =6 AND title = 'The Dark Knight' ORDER BY id DESC;
+        // count() - SELECT COUNT() from movies WHERE id = 6;
+        // getClassName() - Entity name
 
-        return $this->render('movies/index.html.twig', [
-            'movies' => $movies,
-        ]);
+        $repository = $em->getRepository(Movie::class);
+        $movies = $repository->getClassName();
+
+        dd($movies);
+
+        return $this->render('movies/index.html.twig');
     }
 }
